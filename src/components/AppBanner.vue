d93e2a658a25295365b5ceab7858729cc59a538e
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-11-08 10:51:22 +0100

    Scaffolding

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+
+    name: "AppBanner",
+    data() {
+
+        return {
+
+            title: "Banner Title",
+            imgName: "4ll1s0n.png"
+
+        }
+    },
+    methods: {
+
+        getImagePath(imgPath) {
+
+            return new URL(imgPath, import.meta.url).href;
+
+        }
+
+    }
+}
+
+</script>
+
+<template>
+
+    <section>
+       <img :src="getImagePath(`../assets/img/${this.imgName}`)" alt="">
+    </section>
+
+</template>
+
+<style scoped>
+</style>
